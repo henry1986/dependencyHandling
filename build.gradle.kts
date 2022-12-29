5e2aad0e492b09a23ceb8508131763d862b1f0a0
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2022-12-29 13:07:06 +0100

    released to mavenCentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("jvm") version "1.3.70"
     id("com.jfrog.artifactory") version "4.17.2"
     `maven-publish`
+    id("signing")
 }
 
 val dependencyHandlingVersion =
@@ -13,25 +14,66 @@ val dependencyHandlingVersion =
 group = "org.daiv.dependency"
 version = dependencyHandlingVersion
 
-
 repositories {
     mavenCentral()
-    maven("https://artifactory.daiv.org/artifactory/gradle-dev-local")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("com.google.code.gson:gson:2.10")
     implementation("org.daiv.dependency:VersionPluginConfiguration:0.0.15")
-//    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
-//    implementation(kotlin("gradle-plugin"))
-//    implementation(kotlin("reflect", "1.4.10"))
-//    implementation(gradleApi())
 }
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+signing {
+    sign(publishing.publications)
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            pom {
+                packaging = "jar"
+                name.set("DependencyHandling")
+                description.set("the dependencyHandling for the org.daiv components")
+                url.set("https://github.com/henry1986/dependencyHandling")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/henry1986/dependency-versionPlugin/issues")
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/henry1986/dependencyHandling.git")
+                    developerConnection.set("scm:git:https://github.com/henry1986/dependencyHandling.git")
+                    url.set("https://github.com/henry1986/dependencyHandling")
+                }
+                developers {
+                    developer {
+                        id.set("henry86")
+                        name.set("Martin Heinrich")
+                        email.set("martin.heinrich.dresden@gmx.de")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "sonatypeRepository"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials(PasswordCredentials::class)
         }
     }
 }
